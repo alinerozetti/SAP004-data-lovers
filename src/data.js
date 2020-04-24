9901bb0bf274b16cef0cef9494b90dbb80ae99b6
Mariana Barros <mamirandabarros11@gmail.com>
2020-04-24 09:34:11 -0300

    inserçao do pokemon com nome e numero na tela

--- src/data.js
+++ src/data.js
@@ -1,10 +1,20 @@
-// funções de exemplo
+import data from './data/pokemon/pokemon.js';
 
-export const example = () => {
-  return 'example';
+export const listing = () => {
+  const pokemons = data.pokemon.map(poke => {
+    return {
+      img: poke.img,
+      name: poke.name,
+      num: poke.num,
+    }
+  })
+  return pokemons;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+export const nameSearch = (name) => {
+  let pokemons = listing().filter(pokemon => {
+    return pokemon.name === name;
+  })
+  return pokemons;
 };
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import { listing, nameSearch } from './data.js';
 
-console.log(example, data);
-// teste clone
+const pokemons = listing();
+const html = pokemons.map(pokemon => {
+  return `<li><img src="${pokemon.img}" alt="">${pokemon.num}-${pokemon.name}</li>`
+})
+
+document.getElementById("list").innerHTML = html.join("");
+
+
+console.log(nameSearch("Ivysaur"));

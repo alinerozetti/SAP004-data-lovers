1e3b703331a0fec4ef3233e3fd751926d05465ba
Mariana Barros <mamirandabarros11@gmail.com>
2020-05-10 22:45:47 -0300

    melhorando o teste

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,27 +1,35 @@
-// import { example, anotherExample } from '../src/data.js';
-import { typeSearch } from '../src/data.js';
+import { typeSearch, get, orderBy, orderByWeight } from '../src/data.js';
 
-const pokemons = [{
-  "id": 1,
-  "num": "001",
-  "name": "Bulbasaur",
-  "type": [
-    "Grass",
-    "Poison"
-  ]
-}]
+const pokemons = [
+  { "id": 1, "num": "001", "name": "Bulbasaur", "type": ["Grass", "Poison"], "weight": "6.9 kg" },
+  { "id": 41, "num": "041", "name": "Zubat", "type": ["Poison", "Flying"], "height": "0.79 m", }];
 
 describe('pokemon', () => {
-  it("testa o tipo do pokemon", () => {
-    expect(typeSearch(pokemons, "type", "Grass")).toEqual([{
-      "id": 1,
-      "num": "001",
-      "name": "Bulbasaur",
-      "type": [
-        "Grass",
-        "Poison"
-      ]
-    }]);
+  it("testa o tipo", () => {
+    expect(typeSearch(pokemons, "type", "Grass")).toEqual([
+      { "id": 1, "num": "001", "name": "Bulbasaur", "type": ["Grass", "Poison"], "weight": "6.9 kg" }]);
+  });
+
+  it("testa o id", () => {
+    expect(get(pokemons, "id", 1)).toEqual(
+      { "id": 1, "num": "001", "name": "Bulbasaur", "type": ["Grass", "Poison"], "weight": "6.9 kg" });
+  });
+
+  it("testa o num", () => {
+    expect(get(pokemons, "num", "001")).toEqual(
+      { "id": 1, "num": "001", "name": "Bulbasaur", "type": ["Grass", "Poison"], "weight": "6.9 kg" });
+  });
+
+  it("testa a ordenação", () => {
+    expect(orderBy(pokemons, "name")).toEqual([
+      { "id": 1, "num": "001", "name": "Bulbasaur", "type": ["Grass", "Poison"], "weight": "6.9 kg" },
+      { "id": 41, "num": "041", "name": "Zubat", "type": ["Poison", "Flying"], "height": "0.79 m", }]);
+  });
+
+  it("teste ordenação por peso", () => {
+    expect(orderByWeight(pokemons, "weight")).toEqual([
+      { "id": 1, "num": "001", "name": "Bulbasaur", "type": ["Grass", "Poison"], "weight": "6.9 kg" },
+      { "id": 41, "num": "041", "name": "Zubat", "type": ["Poison", "Flying"], "height": "0.79 m", }]);
   });
 });
 
